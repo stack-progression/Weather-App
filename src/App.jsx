68a26635b24fbab756c88f6db547f6df9a782bb8
Andrei <stack.progression@gmail.com>
2026-01-03 02:11:55 +0200

    project outline

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,60 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { AppContext } from "./context/AppContext";
 
 const App = () => {
+  const { location, weather, getDay, getHour, getTempHour } =
+    useContext(AppContext);
+
+  console.log(location);
+  console.log(weather);
+
+  if (!weather) return <div>Loading...</div>;
+
   return (
     <div>
-      
-    </div>
-  )
-}
+      <h1>{weather.location.name}</h1>
 
-export default App
+      <p>
+        {
+          weather?.forecast?.forecastday[0]?.hour[
+            getTempHour(weather?.forecast?.forecastday[0]?.hour[0]?.time_epoch)
+          ]?.temp_c
+        }
+      </p>
+
+      <p>
+        {weather?.forecast?.forecastday[0]?.day?.maxtemp_c}° /{" "}
+        {weather?.forecast?.forecastday[0]?.day?.mintemp_c}° Se simte ca{" "}
+        {weather?.forecast?.forecastday[0]?.day?.avgtemp_c}°
+      </p>
+
+      {weather?.forecast?.forecastday[0]?.hour?.map((hour, index) => {
+        return (
+          <div key={index}>
+            <p>{getHour(hour.time_epoch)}</p>
+            <img src={hour.condition.icon} alt="" />
+            <p>grade: {hour.temp_c}°</p>
+            <p>umiditate: {hour.humidity}%</p>
+          </div>
+        );
+      })}
+
+      <p>urmatoarele zile</p>
+      {weather?.forecast?.forecastday?.map((day, index) => {
+        return (
+          <div key={index}>
+            <div>
+              <p>{getDay(day.date_epoch)}</p>
+            </div>
+            <p>Sanse de ploaie {day.day.daily_chance_of_rain}%</p>
+            <img src={day.day.condition.icon} alt="" />
+            <p>maxtemp_c: {day.day.maxtemp_c}°</p>
+            <p>mintemp_c: {day.day.mintemp_c}°</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/context/Context.jsx
@@ -0,0 +1,94 @@
+import React, { useEffect, useState } from "react";
+import { AppContext } from "./AppContext";
+
+const Context = ({ children }) => {
+  const [location, setLocation] = useState("bucuresti");
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(
+          `https://api.weatherapi.com/v1/forecast.json?key=d7a21304535c41b7a07211753252712&q=${location}&days=14&aqi=yes&alerts=yes`
+        );
+        if (!res.ok) {
+          throw new Error("problem in response");
+        }
+        const data = await res.json();
+        setWeather(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, [location]);
+
+  const getDay = (day) => {
+    if (!day) return "";
+    const date = new Date(day * 1000);
+    const dayOfWeek = date.getDay();
+    const daysOfWeek = [
+      "Duminică",
+      "Luni",
+      "Marți",
+      "Miercuri",
+      "Joi",
+      "Vineri",
+      "Sâmbătă",
+    ];
+    return daysOfWeek[dayOfWeek] || "";
+  };
+
+  const getHour = (h) => {
+    const date = new Date(h * 1000);
+    const hour = date.getHours();
+    const hourDay = [
+      "00:00",
+      "01:00",
+      "02:00",
+      "03:00",
+      "04:00",
+      "05:00",
+      "06:00",
+      "07:00",
+      "08:00",
+      "09:00",
+      "10:00",
+      "11:00",
+      "12:00",
+      "13:00",
+      "14:00",
+      "15:00",
+      "16:00",
+      "17:00",
+      "18:00",
+      "19:00",
+      "20:00",
+      "21:00",
+      "22:00",
+      "23:00",
+      "24:00",
+    ];
+    return hourDay[hour];
+  };
+
+  const getTempHour = (t) => {
+    const date = new Date(t * 1000);
+    const hour = date.getHours();
+    return hour
+  }
+
+  const AppValue = {
+    location,
+    setLocation,
+    weather,
+    setWeather,
+    getDay,
+    getHour,
+    getTempHour,
+  };
+
+  return <AppContext.Provider value={AppValue}>{children}</AppContext.Provider>;
+};
+
+export default Context;
