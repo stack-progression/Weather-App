2cf56bdebfdbac57c80f2ce6ea0ca08df9371995
Andrei <stack.progression@gmail.com>
2026-01-10 21:25:22 +0200

    project updated v1.2

--- src/components/SearchItems/SearchItems.jsx
+++ src/components/SearchItems/SearchItems.jsx
@@ -4,12 +4,12 @@ import { AppContext } from "../../context/AppContext";
 
 const SearchItems = () => {
   
-  const { setLocation, unice, deleteItem } = useContext(AppContext);
+  const { setLocation, unice, deleteItem, setOpen } = useContext(AppContext);
 
   return (
     <div className="search-items">
       {unice.sort((a, b) => b.id - a.id).map((t) => {
-        return <p className="search-items-text" onClick={() => setLocation(t.text)} onDoubleClick={() => deleteItem(t.id)} onTouchMove={() => deleteItem(t.id)} key={t.id}>{t.text}</p>;
+        return <p className="search-items-text" onClick={() => (setLocation(t.text), setOpen(false))} onDoubleClick={() => deleteItem(t.id)} onTouchMove={() => deleteItem(t.id)} key={t.id}>{t.text}</p>;
       })}
     </div>
   );
